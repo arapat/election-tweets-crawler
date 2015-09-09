5e6410a5ab5228d0d59b1fb670682ada04ad6d70
Julaiti Alafate <jalafate@gmail.com>
2015-09-08 21:14:56 -0700

    improve the robustness

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # A simple tweets crawler
+A streaming tweets crawler for collecting tweets related to 2016 US presidential election.
--- crawlElection.py
+++ crawlElection.py
@@ -4,7 +4,10 @@ from tweepy.streaming import StreamListener
 from tweepy import OAuthHandler
 from tweepy import Stream
 
+import os
 import json
+from subprocess import Popen
+from subprocess import PIPE
 
 #Variables that contains the user credentials to access Twitter API 
 access_token = ""
@@ -12,65 +15,33 @@ access_token_secret = ""
 consumer_key = ""
 consumer_secret = ""
 
-"""
-Track keywords:
-
-See http://seed.ucsd.edu/mediawiki/index.php/User:Julaiti_Alafate/Diary#Presidential_Election_2016.
-"""
-
-# Trace 1
-trace1 = ['election 2016', '2016 presidential', 'election 16', 'presidential election', 'presidential candidate',
-          'presidency 2016']
-non_trace1 = ['ukraine', 'egypt', 'russia', 'afghanistan', 'syria', 'turkey', 'benghazi', 'iraq', 'colombia', 
-              'ukrainian', 'indonesia', 'lebanon', 'lebanese', 'nuristani', 'yudhoyono', 'renamo', 'jakarta', 
-              'poroshenko', 'abdullah', 'malawi', 'kenya', 'sunnis', 'tayyip', 'aoun', 'widodo', 'jokowi', 
-              'prabowo', 'kurds', 'mec', 'apc', 'akufo', 'syria', 'assad', 'fattah', 'uganda', '2015', '2011']
-
-# Trace 2
-trace2a = ["joe biden", "lincoln chafee", "hillary clinton", "martin o malley", "bernie sanders", "jim webb"]
-trace2b = ["biden", "chafee", "hillary", "clinton", "o malley", "sanders", "webb"]
-trace2c = ["jeb bush", "ben carson", "chris christie", "ted cruz", "bob ehrlich", "mark everson", "carly fiorina",
-           "jim gilmore", "lindsey graham", "mike huckabee", "bobby jindal", "john kasich", "pete king", 
-           "george pataki", "rand paul", "rick perry", "marco rubio", "rick santorum", "donald trump", 
-           "scott walker"]
-trace2d = ["jeb", "bush", "christie", "cruz", "everson", "fiorina", "huckabee", "jindal", "kasich", "pataki",
-           "rubio", "santorum", "trump" ]
-trace2 = trace2a + trace2b + trace2c + trace2d
-# Trace 3
-trace3 = ["democratic nomination", "republican nomination", "democratic platform", "republican platform"]
+# Trace
+trace = ['donald trump', 'hillary clinton', 'bernie sanders', 'ted cruz', 'jeb bush', 'scott walker', 'ben carson', 'presidential candidate', 'joe biden', 'planned parenthood', 'marco rubio', 'bill clinton', 'rand paul', 'chris christie', 'birthright citizenship', 'presidential election', 'george bush', 'republican presidential', 'paul rand', 'sarah palin', 'john kasich', 'presidential candidates', 'running president', '2016 election', 'presidential campaign', 'gop candidates', 'super pac', 'presidential race', 'gop presidential', "martin o'malley", 'gop candidate', 'hilary clinton', 'republican party', 'elizabeth warren', 'presidential debate', 'next president', 'bush campaign', 'democratic party', 'president 2016', 'cruz 2016', 'democratic presidential', 'gop primary', 'democratic nomination', 'republican nomination'] \
+    + ['trump', 'hillary', 'clinton', 'sanders', 'bernie', 'jeb', 'gop', 'biden', 'christie', 'republican', 'hillaryclinton', 'tedcruz', 'republicans', 'huckabee', 'berniesanders', 'democrats', 'jindal', 'jebbush', 'health', 'democrat', 'sensanders']
 
 #This is a basic listener that just prints received tweets to stdout.
 class StdOutListener(StreamListener):
 
+    def __init__(self):
+      self.threshold = 13100
+      self.counter = 0
+
     def on_data(self, data):
         try:
-            raw_text = json.loads(data)['text']
+            s = os.statvfs('/')
+            if s.f_bavail <= self.threshold:
+              self.counter = self.counter + 1
+              filename = 'election%03d' % self.counter
+              command = "aws s3 cp tweets s3://twitter-collect/data/%s" % filename
+              process = Popen(command, shell=True, stdout=PIPE)
+              process.wait()
+              process = Popen("rm tweets", shell=True, stdout=PIPE)
+              process.wait()
+            json.loads(data)
+            with open('tweets', 'a') as f:
+              f.write(data)
         except:
-            return True
-        text = raw_text.lower()
-        valid = False
-        # Trace 2 and 3
-        for trace in trace2 + trace3:
-            keywords = trace.split()
-            contains = [text.find(kw) >= 0 for kw in keywords]
-            if all(contains):
-                # print "Trace 2/3:", keywords
-                valid = True
-                break
-        # Trace 1
-        if not valid:
-            missed = [text.find(keyword) < 0 for keyword in non_trace1]
-            if all(missed):
-                for trace in trace1:
-                    keywords = trace.split()
-                    contains = [text.find(kw) >= 0 for kw in keywords]
-                    if all(contains):
-                        # print "Trace 1", keywords
-                        valid = True
-                        break
-        if valid:
-            # print raw_text
-            print data,
+            pass
         return True
 
     def on_error(self, status):
@@ -85,6 +56,9 @@ if __name__ == '__main__':
     auth.set_access_token(access_token, access_token_secret)
     stream = Stream(auth, l)
 
-    #This line filter Twitter Streams to capture data by the keywords: 'python', 'javascript', 'ruby'
-    
-    stream.filter(track=trace1 + trace2 + trace3)
+    while True:
+      try:
+        stream.filter(track=trace)
+      except:
+        pass
+
