62ac3d5307c10e5ead695892aa6b62a87793387f
Julaiti Alafate <jalafate@gmail.com>
2016-03-09 17:56:56 -0800

    replaced system commandline with boto3 for uploading to s3

--- crawlElection.py
+++ crawlElection.py
@@ -1,64 +1,71 @@
-# Import the necessary methods from tweepy library
+import json
+import os
+import sys
+
+import boto3
 from tweepy.streaming import StreamListener
 from tweepy import OAuthHandler
 from tweepy import Stream
 
-import os
-import sys
-import json
-from subprocess import Popen
-from subprocess import PIPE
+# a file in which we store the keywords for filtering the streaming result
+TRACK_KEYWORDS_FILE_NAME = "track-mar03.txt"
 
-COUNTER_INIT = 0
-TRACK_FILE = 'track-mar03.txt'
+# The streaming data will be saved locally until the the size of the local
+# file exceeds MAXIMUM_FILE_SIZE. Then we uploaded the local file to AWS S3,
+# rename it to REMOTE_FILE_NAME + sequential number, and delete the local file.
+LOCAL_FILE_NAME = "tweets"
+REMOTE_FILE_NAME = "tweets/stream"
+REMOTE_FILE_NAME_STARTING_NUM = 0
+MAXIMUM_FILE_SIZE = 4 * 1024 * 1024 * 1024  # local file won't exceed 4 GB
 
-# Variables that contains the user credentials to access Twitter API
-access_token = ""
-access_token_secret = ""
-consumer_key = ""
-consumer_secret = ""
+# Twitter API user credentials
+TWITTER_ACCESS_TOKEN = ""
+TWITTER_TOKEN_SECRET = ""
+TWITTER_CONSUMER_KEY = ""
+TWITTER_CONSUMER_SECRET = ""
+
+# Amazon S3 user credentials
+AWS_ACCESS_KEY = ""
+AWS_SECRET_KEY = ""
+AWS_REGION = ""
+AWS_BUCKET_NAME = ""
 
 # Track
 track = []
 
 
+def show_message(message):
+    sys.stdout.write(message + "\n\n")
+    sys.stdout.flush()
+
+
 def read_track():
-    with open(TRACK_FILE) as f:
+    with open(TRACK_KEYWORDS_FILE_NAME) as f:
         for w in f:
             track.append(w.strip())
-    print 'KEYWORDS'
-    print '============================='
-    print ', '.join(track)
-    print '============================='
-    print "Number of keywords:", len(track)
-    print
+    show_message("KEYWORDS:")
+    show_message(", ".join(track))
+    show_message("Number of keywords: %d" % len(track))
 
 
 class StdOutListener(StreamListener):
-
-    def __init__(self):
-        self.filename = 'tweets'
-        self.threshold = 4 * 1024 * 1024 * 1024
-        self.counter = COUNTER_INIT
-        self.bucketName = ''
+    def __init__(self, s3):
+        self.s3 = s3
+        self.counter = REMOTE_FILE_NAME_STARTING_NUM
 
     def on_data(self, data):
-        if (os.path.isfile(self.filename) and
-                os.stat(self.filename).st_size >= self.threshold):
+        if (os.path.isfile(LOCAL_FILE_NAME) and
+                os.stat(LOCAL_FILE_NAME).st_size >= MAXIMUM_FILE_SIZE):
             self.counter = self.counter + 1
-            filename = 'election%03d' % self.counter
-            sys.stdout.write('uploading %s...\n' % (filename))
-            sys.stdout.flush()
-            command = ("aws s3 cp %s s3://%s/data/%s" %
-                       (self.filename, self.bucketName, filename))
-            process = Popen(command, shell=True, stdout=PIPE)
-            process.wait()
-            os.remove(self.filename)
-            sys.stdout.write('upload done.\n')
+            key = "%s%03d" % (REMOTE_FILE_NAME, self.counter)
+            show_message('Uploading %s...' % (key))
+            self.s3.upload_file(LOCAL_FILE_NAME, AWS_BUCKET_NAME, key)
+            os.remove(LOCAL_FILE_NAME)
+            sys.stdout.write('Upload done.')
             sys.stdout.flush()
 
         json.loads(data)
-        with open(self.filename, 'a') as f:
+        with open(LOCAL_FILE_NAME, 'a') as f:
             f.write(data)
         return True
 
@@ -67,19 +74,26 @@ class StdOutListener(StreamListener):
 
 
 if __name__ == '__main__':
+    # AWS authentication
+    s3 = boto3.client(
+        's3',
+        region_name=AWS_REGION,
+        aws_access_key_id=AWS_ACCESS_KEY,
+        aws_secret_access_key=AWS_SECRET_KEY
+    )
+
+    # Twitter authentication
+    auth = OAuthHandler(TWITTER_CONSUMER_KEY, TWITTER_CONSUMER_SECRET)
+    auth.set_access_token(TWITTER_ACCESS_TOKEN, TWITTER_TOKEN_SECRET)
+    listener = StdOutListener(s3)
+    stream = Stream(auth, listener)
 
     read_track()
-    print 'Start streaming...'
-
-    # This handles Twitter authentication and
-    # the connection to Twitter Streaming API
-    l = StdOutListener()
-    auth = OAuthHandler(consumer_key, consumer_secret)
-    auth.set_access_token(access_token, access_token_secret)
-    stream = Stream(auth, l)
-
     while True:
         try:
+            show_message('(Re)start streaming...')
             stream.filter(track=track)
         except Exception as e:
-            print e
+            # Keep the crawler running without being interupted by exceptions
+            show_message("Exception (skipped):")
+            show_message(e)
